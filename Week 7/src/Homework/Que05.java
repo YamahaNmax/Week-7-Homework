20ebc3baf85ce75358e0f47e5d77322633a53c37
YamahaNmax <m4mitzlearning@gmail.com>
2023-10-29 20:11:54 +0000

    new

--- Week 7/src/Homework/Que05.java
+++ Week 7/src/Homework/Que05.java
@@ -4,4 +4,37 @@
 package Homework;
 
 public class Que05 {
+
+    // Two instance methods
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int subtract(int x, int y) {
+        return x - y;
+    }
+
+    // Two static methods
+    public static int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public static double divide(double x, double y) {
+        return x / y;
+    }
+
+    public static void main(String[] args) {
+        Que05 calc = new Que05();
+
+        int sum = calc.add(12, 6);
+        int sub = calc.subtract(12, 6);
+        int mlt = multiply(12, 6);
+        double dvd = divide(12, 6);
+
+        // Using string concatenation to display the results
+        System.out.println("Sum of 10 and 5: " + sum);
+        System.out.println("Subtraction between 10 and 5: " + sub);
+        System.out.println("Multiplication of 10 and 5: " + mlt);
+        System.out.println("Division of 10 by 5: " + dvd);
+    }
 }
---
+++ Week 7/src/Homework/Que06.java
@@ -0,0 +1,8 @@
+// Write a program to enter any radius value of the circle and find out the area. (Formula of Area A=PI*r*r).
+
+package Homework;
+
+public class Que06 {
+
+
+}
